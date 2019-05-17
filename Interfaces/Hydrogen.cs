1bf7d974c724240fc2d4291f935ec3a8a6da7f7e
PmaiWoW <PmaiWoW@Gmail.com>
2019-05-17 12:14:48 +0100

    Program to test implementation of interfaces

---
+++ Interfaces/Hydrogen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public class Hydrogen : IFuel
+    {
+        public float Efficiency => 12.3f;
+
+        public void Burn()
+        {
+            Console.WriteLine("Hydrogen is burning!");
+        }
+    }
+}
---
+++ Interfaces/IFuel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IFuel
+    {
+        // Método da interface, nenhum método/propriedade de interfaces
+        // tem corpo e são public abstract por omissão
+        float Efficiency { get; }
+        void Burn(); 
+    }
+}
